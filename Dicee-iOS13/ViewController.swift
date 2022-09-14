908e8efa9ff88edea1375e1267b2ad1d7eb09bcd
niramay447 <nk447@snu.edu.in>
2022-09-14 13:28:31 +0530

    align & pinning

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
         
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = diceArray[Int.random(in:1...6)]
-        diceImageView2.image = diceArray[Int.random(in:1...6)]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
         
     }
